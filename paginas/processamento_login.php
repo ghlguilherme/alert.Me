1e90990052159b10efa189436b79f0c3ed107296
Guilherme Henrique Lourenço <ghl.guilherme@gmail.com>
2016-08-25 09:07:54 -0300

    segurança no login

--- paginas/processamento_login.php
+++ paginas/processamento_login.php
@@ -3,12 +3,12 @@
  $usuario  = isset($_POST['txt-usuario']) ? $_POST['txt-usuario'] : null;
  $senha = isset($_POST['txt-senha']) ? $_POST['txt-senha'] : null;
 
- $sql_login = "SELECT PESSOA_USUARIO FROM PESSOA WHERE PESSOA_USUARIO = '".escape_bd($usuario)."' AND PESSOA_SENHA = '".md5(escape_bd($senha))."'";
+ $sql_login = "SELECT PESSOA_USUARIO, PESSOA_SENHA FROM PESSOA WHERE PESSOA_USUARIO = '".escape_bd($usuario)."' AND PESSOA_SENHA = '".md5(escape_bd($senha))."'";
 
  $result = mysqli_query(conecta_bd(), $sql_login);
  
  while($tupla = mysqli_fetch_row($result)){
-     if($tupla[0] == $usuario){
+     if($tupla[0] == $usuario && $tupla[1] == md5($senha)){
          echo "success";
      }else{
          echo "erro";
